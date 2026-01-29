4d482906ab70948d0418f9880c1b415521f32dfa
Ashwini Gowda <ashwinigowda975@gmail.com>
2026-01-29 13:14:32 +0530

    Add files via upload

---
+++ .github/workflows/script.js
@@ -0,0 +1,45 @@
+<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+$(document).ready(function() {
+
+  var
+    words = $('#tagi text'),
+    l = words.length,
+    current = null,
+    delay = 2000;
+
+  function clearBlink(o) {
+    var
+      ca = o.getAttribute('class').split(' '),
+      i = ca.indexOf('blink');
+
+    if (i !== -1) {
+      ca.splice(i, 1);
+      o.setAttribute('class', ca.join(' '));
+    }
+  }
+
+  function addBlink(o) {
+    var
+      ca = o.getAttribute('class').split(' ');
+    ca.push('blink');
+    o.setAttribute('class', ca.join(' '));
+  }
+
+  function wordblink() {
+
+    var e;
+
+    if (current !== null) {
+      clearBlink(words.eq(current)[0])
+    }
+
+    current = Math.floor(Math.random() * l);
+    e = words.eq(current);
+    addBlink(e[0]);
+
+    setTimeout(wordblink, delay);
+  }
+
+  wordblink();
+
+});
